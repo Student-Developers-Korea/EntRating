125c20cfa2fa59476f41f50f79641999492d2da6
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-08-11 20:50:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ async function getcontent(id){
   return b.content
 }
 
+function left(){
+  if($('#score')[0].value > 1){
+    $('#score')[0].value -= 1
+  }
+}
+
+function right(){
+  if($('#score')[0].value < 5){
+    $('#score')[0].value += 1
+  }
+}
+
 const gradebox = `<style>
   @font-face {font-family: 'GmarketSansMedium';src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');font-weight: normal;font-style: normal;}
   #gradebox{
