802902fafbea5b815d5a030328279a5d780311e7
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-08-15 12:49:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ $(document).ready(async function(){
     for(var i = 0; i<12; i++){
       if(b.indexOf(a[i])==-1){
         var c = '없음'
+        console.log(c)
         } else{
             var c = await fetch(`https://playentry.org/api/discuss/find?commentsNothing=false&sort=created&rows=20&page=${parseInt(b.indexOf(a[i]/20))+1}&category=avo&noCache=1570785797940`)
             var c = await c.json()
